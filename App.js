a4ec4ccf0e8076b82440aaf15830653c7903a859
youssef ABBADI <youssefabbadipro@gmail.com>
2023-05-31 13:42:35 +0200

    Logo DONE

--- App.js
+++ App.js
@@ -10,7 +10,9 @@ import MyTabsAdmin from './MyApp/Administrator/MyTabsAdmin';
 
 import LoginScreen from './MyApp/Worker/Login_InscriptionScreens/LoginScreenWorker';
 import LoginAdminScreen from './MyApp/Administrator/LoginScreenAdmin';
+
 import Chose from './MyApp/ChoseNavigation'
+import logo from './MyApp/logo'
 
 const Stack = createNativeStackNavigator();
 
@@ -18,7 +20,8 @@ function App() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator>
-        <Stack.Screen options={{headerLeft: null}} name="Qui êtes-vous ?" component={Chose} />
+        <Stack.Screen options={{headerShown: false}} name="logo" component={logo} />
+        <Stack.Screen options={{headerShown: false}} name="Qui êtes-vous ?" component={Chose} />
         <Stack.Screen options={{headerLeft: null}} name="LoginEmploye" component={LoginScreen} />
         <Stack.Screen options={{headerLeft: null}} name="LoginAdmin" component={LoginAdminScreen} />
         <Stack.Screen name="Inscription" component={InscriptionScreen} />
